a9baf7afcf5fbbc6237710adf82ac989c7e353f1
felixpepa <felix@pepamarket.com>
2023-05-20 13:25:08 -0700

    [feat] add tiled map to phaser scene

--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -21,6 +21,24 @@ export default class Level extends Phaser.Scene {
 
 	editorCreate(): void {
 
+		// main1
+		const main1 = this.add.tilemap("main1");
+		main1.addTilesetImage("bundle3", "bundle2");
+		main1.addTilesetImage("bundle2", "bundle1");
+		main1.addTilesetImage("AutoMap Rules", "guapen");
+
+		// main
+		const main = this.add.tilemap("main1");
+		main.addTilesetImage("bundle3", "bundle1");
+		main.addTilesetImage("bundle2", "bundle2");
+		main.addTilesetImage("AutoMap Rules", "guapen");
+
+		// main_1
+		const main_1 = this.add.tilemap("main1");
+		main_1.addTilesetImage("bundle3", "bundle2");
+		main_1.addTilesetImage("bundle2", "bundle1");
+		main_1.addTilesetImage("AutoMap Rules", "guapen");
+
 		// fufuSuperDino
 		const fufuSuperDino = this.add.image(640, 257, "FufuSuperDino");
 
@@ -31,13 +49,48 @@ export default class Level extends Phaser.Scene {
 		new PushActionScript(onPointerDownScript);
 
 		// text
-		const text = this.add.text(640, 458, "", {});
+		const text = this.add.text(640, 468, "", {});
 		text.setOrigin(0.5, 0.5);
 		text.text = "Phaser 3 + Phaser Editor 2D\nWebpack + TypeScript";
 		text.setStyle({ "align": "center", "fontFamily": "Arial", "fontSize": "3em" });
 
+		// container_1
+		const container_1 = this.add.container(0, -3);
+
+		// land
+		const land = main1.createLayer("Land", ["bundle3"], 0, 0);
+		land.scaleX = 0.3;
+		land.scaleY = 0.3;
+		container_1.add(land);
+
+		// deco_1
+		const deco_1 = main.createLayer("Deco", ["bundle3"], 0, 0);
+		deco_1.scaleX = 0.3;
+		deco_1.scaleY = 0.3;
+		container_1.add(deco_1);
+
+		// house_1
+		const house_1 = main_1.createLayer("House", ["bundle2"], 0, 0);
+		house_1.scaleX = 0.3;
+		house_1.scaleY = 0.3;
+		container_1.add(house_1);
+
+		// houseFront_1
+		const houseFront_1 = main_1.createLayer("HouseFront", ["bundle2"], 0, 0);
+		houseFront_1.scaleX = 0.3;
+		houseFront_1.scaleY = 0.3;
+		container_1.add(houseFront_1);
+
+		this.main1 = main1;
+		this.main = main;
+		this.main_1 = main_1;
+
 		this.events.emit("scene-awake");
 	}
+
+	private main1!: Phaser.Tilemaps.Tilemap;
+	private main!: Phaser.Tilemaps.Tilemap;
+	private main_1!: Phaser.Tilemaps.Tilemap;
 
 	/* START-USER-CODE */
 
