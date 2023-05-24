cf684bb3eade60b63b33da93269b10c4d171c1b8
felixpepa <felix@pepamarket.com>
2023-05-23 22:35:17 -0700

    [feat] add player and movement script

---
+++ src/components/Physics.ts
@@ -0,0 +1,37 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Physics {
+
+	constructor(gameObject: Phaser.GameObjects.Image) {
+		this.gameObject = gameObject;
+		(gameObject as any)["__Physics"] = this;
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		const scene = this.gameObject.scene;
+		scene.physics.add.existing(this.gameObject);
+		/* END-USER-CTR-CODE */
+	}
+
+	static getComponent(gameObject: Phaser.GameObjects.Image): Physics {
+		return (gameObject as any)["__Physics"];
+	}
+
+	private gameObject: Phaser.GameObjects.Image;
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
