aabdfbd01377438c53543831044a1a1044fb15e4
felixpepa <felix@pepamarket.com>
2023-06-01 20:20:13 -0700

    [UI] add Information panel for Shop UI

--- src/web/ShopUI.tsx
+++ src/web/ShopUI.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import ItemsGrid from "./ItemsGrid";
 import getItems from "../utils/getItems";
 import ItemsContext from "./ItemsContext";
+import ItemInformation from "./ItemInformation";
 import {
   goUp,
   goLeft,
@@ -10,6 +11,8 @@ import {
   goDown,
   getIndexFromMaxtrixPosition,
 } from "../utils/keyboardNavigation";
+
+// import linkImage from "./assets/bg.png";
 
 function ShopUI() {
   const [itemSelected, setItemSelected] = useState(0);
@@ -55,7 +58,29 @@ function ShopUI() {
             <ItemsGrid items={getItems()} />
           </ItemsContext.Provider>
         </div>
-        <div className="w-full xl:w-1/2"></div>
+        <div className="flex flex-col items-center self-end xl:items-start w-full xl:w-1/2 my-6 xl:my-0">
+          {/* <img
+            className="absolute hidden xl:block top-0 ml-48 z-0"
+            src={linkImage}
+            alt="link"
+          /> */}
+          {/* Add text component */}
+            <a href="#">    
+                <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            </a>
+            <div className="p-5">
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+
+          <ItemInformation />
+        </div>
       </div>
     </div>
   );
