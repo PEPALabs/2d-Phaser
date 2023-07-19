05116695d12567dbb2381a7cc2960ec924e3ff9f
felix <felix@pepamarket.com>
2023-07-18 17:52:27 -0700

    [ui] Add stats display card that display user level; add scrollable plant list to plants; complete functionalities of buttons in resource panel

--- src/data/plants.ts
+++ src/data/plants.ts
@@ -1,10 +1,11 @@
-
+import usePlantStore from "./plantStore";
 
 export type PlantState = "EMPTY" | "PLANTING" | "READY";
 
 export type PlantType = {
     id: number;
     name: string;
+    description: string;
     state: PlantState;
     duration: number; // in ms
     seedTexture: string;
@@ -13,25 +14,36 @@ export type PlantType = {
 
 export var plantData:PlantType[] = [
     {
-        id: 1,
-        name: "Guapen",
+        id: 0,
+        name: "Wheat",
+        description: "Plant Type Wheat: Generates 100 wheat",
         state: "EMPTY",
+        duration: 10000,
+        seedTexture: "guapen",
+        plantTexture: "FufuSuperDino",
+    },
+    {
+        id: 1,
+        name: "Grass",
+        state: "EMPTY",
+        description: "Plant Type Grass: Generates 1 Grass",
         duration: 30000,
         seedTexture: "guapen",
         plantTexture: "FufuSuperDino",
     },
     {
         id: 2,
-        name: "Guapen",
+        name: "Grass",
+        description: "Plant Type Grass: Generates 2 Grass",
         state: "PLANTING",
-
         duration: 20000,
         seedTexture: "guapen",
         plantTexture: "FufuSuperDino",
     },
     {
         id: 3,
-        name: "Guapen",
+        name: "Apple",
+        description: "Plant Type Apple: Generates 4 Apple",
         state: "READY",
         duration: 40000,
         seedTexture: "guapen",
@@ -46,4 +58,9 @@ export var plants = {
         seedTexture: "guapen",
         plantTexture: "FufuSuperDino",
     }
+}
+
+usePlantStore.getState().populateId(20);
+for(var i = 0; i < plantData.length; i++){
+    usePlantStore.getState().addPlant(plantData[i], String(plantData[i].id));
 }
