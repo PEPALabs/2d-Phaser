d0b6d3f58f5f7d343bc9d9e48b6894b684e80ed5
Felix Pepa <felix@pepamarket.com>
2023-08-24 05:12:37 +0000

    [game] add map assets

---
+++ static/assets/tiled/clothing/clothing store.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="clothing store" tilewidth="4000" tileheight="4000" tilecount="33" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="4000" height="4000" source="png/clothing-store_0000_pepa.png"/>
+ </tile>
+ <tile id="1">
+  <image width="4000" height="4000" source="png/clothing-store_0001_vanity-mirror2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="4000" height="4000" source="png/clothing-store_0002_vanity-mirror1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="4000" height="4000" source="png/clothing-store_0003_Clothes-on-the-wall.png"/>
+ </tile>
+ <tile id="4">
+  <image width="4000" height="4000" source="png/clothing-store_0004_vase.png"/>
+ </tile>
+ <tile id="5">
+  <image width="4000" height="4000" source="png/clothing-store_0005_Bags.png"/>
+ </tile>
+ <tile id="6">
+  <image width="4000" height="4000" source="png/clothing-store_0006_Cashier-clothes.png"/>
+ </tile>
+ <tile id="7">
+  <image width="4000" height="4000" source="png/clothing-store_0007_Cashier.png"/>
+ </tile>
+ <tile id="8">
+  <image width="4000" height="4000" source="png/clothing-store_0008_door.png"/>
+ </tile>
+ <tile id="9">
+  <image width="4000" height="4000" source="png/clothing-store_0009_goods-shelves2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="4000" height="4000" source="png/clothing-store_0010_hanger2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="4000" height="4000" source="png/clothing-store_0011_hanger1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="4000" height="4000" source="png/clothing-store_0012_fitting-room-wall2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="4000" height="4000" source="png/clothing-store_0013_curtain.png"/>
+ </tile>
+ <tile id="14">
+  <image width="4000" height="4000" source="png/clothing-store_0014_pole2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="4000" height="4000" source="png/clothing-store_0015_small-chair.png"/>
+ </tile>
+ <tile id="16">
+  <image width="4000" height="4000" source="png/clothing-store_0016_mirror.png"/>
+ </tile>
+ <tile id="17">
+  <image width="4000" height="4000" source="png/clothing-store_0017_fitting-room-wall.png"/>
+ </tile>
+ <tile id="18">
+  <image width="4000" height="4000" source="png/clothing-store_0018_curtain.png"/>
+ </tile>
+ <tile id="19">
+  <image width="4000" height="4000" source="png/clothing-store_0019_pole.png"/>
+ </tile>
+ <tile id="20">
+  <image width="4000" height="4000" source="png/clothing-store_0020_Small-chair.png"/>
+ </tile>
+ <tile id="21">
+  <image width="4000" height="4000" source="png/clothing-store_0021_mirror.png"/>
+ </tile>
+ <tile id="22">
+  <image width="4000" height="4000" source="png/clothing-store_0022_wall.png"/>
+ </tile>
+ <tile id="23">
+  <image width="4000" height="4000" source="png/clothing-store_0023_model.png"/>
+ </tile>
+ <tile id="24">
+  <image width="4000" height="4000" source="png/clothing-store_0024_Model-booth.png"/>
+ </tile>
+ <tile id="25">
+  <image width="4000" height="4000" source="png/clothing-store_0025_chair3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="4000" height="4000" source="png/clothing-store_0026_chair2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="4000" height="4000" source="png/clothing-store_0027_goods-shelves1.png"/>
+ </tile>
+ <tile id="28">
+  <image width="4000" height="4000" source="png/clothing-store_0028_Blue-hanger.png"/>
+ </tile>
+ <tile id="29">
+  <image width="4000" height="4000" source="png/clothing-store_0029_Red-hanger.png"/>
+ </tile>
+ <tile id="30">
+  <image width="4000" height="4000" source="png/clothing-store_0030_chair1.png"/>
+ </tile>
+ <tile id="31">
+  <image width="4000" height="4000" source="png/clothing-store_0031_floor.png"/>
+ </tile>
+ <tile id="32">
+  <image width="4000" height="4000" source="png/clothing-store_0032_bg.png"/>
+ </tile>
+</tileset>
---
+++ static/assets/tiled/seed-market/SeedMarket.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="SeedMarketScene" tilewidth="4000" tileheight="4000" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="4000" height="4000" source="png/Basket1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="4000" height="4000" source="png/CheckoutTable.png"/>
+ </tile>
+ <tile id="4">
+  <image width="4000" height="4000" source="png/IceCab1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="4000" height="4000" source="png/IceCab2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="4000" height="4000" source="png/Outdoor.png"/>
+ </tile>
+ <tile id="7">
+  <image width="4000" height="4000" source="png/Pepa1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="4000" height="4000" source="png/Pepa2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="4000" height="4000" source="png/Rug.png"/>
+ </tile>
+ <tile id="10">
+  <image width="4000" height="4000" source="png/Shelf1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="4000" height="4000" source="png/Shelf2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="4000" height="4000" source="png/Shelf3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="4000" height="4000" source="png/Shelf4.png"/>
+ </tile>
+ <tile id="14">
+  <image width="4000" height="4000" source="png/Shelf5.png"/>
+ </tile>
+ <tile id="15">
+  <image width="4000" height="4000" source="png/Shelf6.png"/>
+ </tile>
+ <tile id="16">
+  <image width="4000" height="4000" source="png/WallLight.png"/>
+ </tile>
+ <tile id="17">
+  <image width="4000" height="4000" source="png/Walls.png"/>
+ </tile>
+ <tile id="18">
+  <image width="4000" height="4000" source="png/WoodFloor.png"/>
+ </tile>
+ <tile id="19">
+  <image width="4000" height="4000" source="png/Background.png"/>
+ </tile>
+ <tile id="20">
+  <image width="306" height="202" source="png/Basket2.png"/>
+ </tile>
+</tileset>
