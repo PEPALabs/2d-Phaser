c7168d9e3b000845dd44bdd2f0eb341731364222
Felix Pepa <felix@pepamarket.com>
2023-08-30 06:18:35 +0000

    change default password requirement to false

--- src/web/shared/env.ts
+++ src/web/shared/env.ts
@@ -1,6 +1,6 @@
 const env = {
   BASE_API_PATH: process.env.BASE_API_PATH,
-  REQUIRED_PASSWORD: process.env.REQUIRED_PASSWORD
+  REQUIRED_PASSWORD: process.env.REQUIRED_PASSWORD || false
 }
 
 export default env
